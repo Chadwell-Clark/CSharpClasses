41507d0ecb1c790850ac0b5f01e6ded1edfc5bfd
Chadwell Clark <wcclark23@comcast.net>
2021-05-26 15:02:12 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of a company. Name it whatever you like.
+            Company myCompany = new Company("See Company", new DateTime(1995, 03, 05));
+
+            // Create three employees
+            Employee employee1 = new Employee("Leroy", "")
+
+
+            // Assign the employees to the company
+
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
+        }
+    }
+}
